9637fcd4461ffcfdb291bcbc37ef6edb6c6b0f53
Matheus Torreão <mtorreao1@gmail.com>
2024-12-11 02:25:05 +0000

    finish course

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,22 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
 
 export default NextAuth(authConfig).auth;
 
+// export default async function middleware(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   requestHeaders.set("x-pathname", request.nextUrl.pathname);
+
+//   let response = NextResponse.next({
+//     request: {
+//       headers: requestHeaders,
+//     },
+//   });
+//   return NextAuth(authConfig).auth;
+// }
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*3000.app.github.dev", "localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
